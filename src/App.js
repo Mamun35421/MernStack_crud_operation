eea7b236bc037f5c0f08a1f11489f91d2dbddf21
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-02-06 12:53:26 +0600

    Register form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import {  Routes, Route } from "react-router-dom";
+import Register from './Components/Register';
+import Edit from './Components/Edit';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <>
+   
+     <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/register" element={<Register/>}/>
+        <Route exact path="/edit/id" element={<Edit/>}/>
+       
+       
+      </Routes>
+   
+     </>
     </div>
   );
 }
